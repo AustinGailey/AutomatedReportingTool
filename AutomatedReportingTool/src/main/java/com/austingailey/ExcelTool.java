e1f563bb400dd46aad86e7d19aa50713028af558
Austin Gailey <austingailey@yahoo.com>
2020-09-07 09:08:58 -0600

    Adding Create Sheet and WB Cloning

--- AutomatedReportingTool/src/main/java/com/austingailey/ExcelTool.java
+++ AutomatedReportingTool/src/main/java/com/austingailey/ExcelTool.java
@@ -3,32 +3,62 @@ package com.austingailey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.File;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+//import org.apache.poi.ss.usermodel.Sheet;
 
 public class ExcelTool {
-    protected Logger log = LoggerFactory.getLogger(EmailTool.class);
+    protected Logger log = LoggerFactory.getLogger(ExcelTool.class);
     protected FileOutputStream out;
     protected String outputPath;
-    protected XSSFWorkbook wb;
+    protected Workbook wb;
 
     public ExcelTool(){}
 
-    public void createWorkBook(String outputPathInput){
-        outputPath = outputPathInput;
+    public void createWorkbook(String filePath){
+        outputPath = filePath;
         openFileOutputStream(false);
         wb = new XSSFWorkbook();
+        wb.createSheet("Default");
+        closeFileOutputStream();
+    }
+
+    public void openExistingWorkbook(String filePath){
+        try{
+            FileInputStream inp = new FileInputStream(filePath);
+            outputPath = filePath;
+            wb = WorkbookFactory.create(inp);
+        }catch(Exception e){
+            createWorkbook(filePath);
+            log.error("Couldn't get existing WorkBook: " + e);
+        }
+    }
+
+    public Workbook getWorkbook(){
+        return wb;
+    }
+
+    public void copyWorkbook(Workbook wbToCopy){
+        openFileOutputStream(false);
+        wb = wbToCopy;
+        closeFileOutputStream();
+    }
+
+    public void createSheet(String sheetName){
+        openFileOutputStream(false);
+        wb.createSheet(sheetName);
         closeFileOutputStream();
     }
 
     public void openFileOutputStream(boolean append){
         try{
-            if(!append){
-                new File(outputPath);
-            }
-            out = new FileOutputStream(outputPath, append);
+            new File(outputPath);
+            out = new FileOutputStream(outputPath);
         }catch(Exception e){
-            log.error("Error opening file" + e);
+            log.error("Error opening file at: " + outputPath + " " +e);
         }
     }
 
@@ -36,6 +66,9 @@ public class ExcelTool {
         try{
             wb.write(out);
             out.close();
-        }catch(Exception e){}
+        }catch(Exception e){
+            log.error("Error writing to file: " + e);
+        }
     }
+
 }
